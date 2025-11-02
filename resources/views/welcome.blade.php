e7cd034337cb3e0cb14a0a78cb3d281e43146924
rajibbhuiyan-afk <rajib.bhuiyan@biometrics-bd.com>
2025-11-02 17:58:10 +0600

    fronend first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel + React App</title>
 
-<h1>laravel</h1>
+    {{-- 👉 React + Vite integration এখানে --}}
+   <!-- @viteReactRefresh -->
+    @vite('resources/js/app.jsx')
+  </head>
+
+  <body>
+    {{-- React অ্যাপ এখানে মাউন্ট হবে --}}
+    <div id="app"></div>
+  </body>
+</html>
